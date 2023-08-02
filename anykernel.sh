88dee4a6ca150a92d56a1ca0b1c17281532c975f
Pzqqt <821026875@qq.com>
2023-08-02 14:40:22 +0800

    AK3: Some adjustments

--- anykernel.sh
+++ anykernel.sh
@@ -98,27 +98,30 @@ mkfs_erofs() {
 
 is_mounted() { mount | grep -q " $1 "; }
 
-snapshot_not_ready=false
-
-if $BOOTMODE; then
-	ls -1 /dev/block/mapper/ | grep -qE '\-cow$' && snapshot_not_ready=true
-else
-	is_mounted /system && umount /system
-	if [ -f /system/bin/bootctl ]; then
-		if [ "$(/system/bin/bootctl get-snapshot-merge-status)" != "none" ]; then
-			snapshot_not_ready=true
+# Check snapshot status
+# Technical details: https://blog.xzr.moe/archives/30/
+snapshot_status=$(${bin}/snapshotupdater_static dump 2>/dev/null | grep '^Update state:' | awk '{print $3}')
+[ $? == 0 ] || {
+	if ! $BOOTMODE; then
+		is_mounted /system && umount /system
+		if [ -f /system/bin/bootctl ]; then
+			snapshot_status=$(/system/bin/bootctl get-snapshot-merge-status)
 		fi
 	fi
-fi
+}
 
-if $snapshot_not_ready; then
+if [ "$snapshot_status" != "none" ]; then
 	ui_print " "
 	ui_print "Seems like you just installed a rom update."
 	ui_print "Please reboot at least once before attempting to install!"
+	if [ "$snapshot_status" == "merging" ]; then
+		ui_print "If this error keeps appearing, please use the rom for a while."
+	fi
 	abort "Aborting..."
 fi
-unset snapshot_not_ready
+unset snapshot_status 
 
+# Check vendor_dlkm partition status
 [ -d /vendor_dlkm ] || mkdir /vendor_dlkm
 is_mounted /vendor_dlkm || \
 	mount /vendor_dlkm -o ro || mount /dev/block/mapper/vendor_dlkm${slot} /vendor_dlkm -o ro || \
@@ -139,26 +142,23 @@ umount /vendor_dlkm
 $BOOTMODE || setenforce 0
 
 ui_print " "
-# if false; then  # DEBUG
 if $skip_update_flag; then
 	ui_print "- No need to update /vendor_dlkm partition."
 else
-	extract_vendor_dlkm_dir=${home}/_extract_vendor_dlkm
-	mkdir -p $extract_vendor_dlkm_dir
-
+	# Dump vendor_dlkm partition image
 	dd if=/dev/block/mapper/vendor_dlkm${slot} of=${home}/vendor_dlkm.img
 
+	# Backup kernel and vendor_dlkm image
 	if $do_backup_flag; then
 		ui_print "- It looks like you are installing Melt Kernel for the first time."
 		ui_print "- Next will backup the kernel and vendor_dlkm partitions..."
 
 		backup_package=/sdcard/Melt-restore-kernel-$(date +"%Y%m%d-%H%M%S").zip
-		rm -rf ${home}/tmp
-		mkdir -p ${home}/tmp
-		mv ${home}/_restore_anykernel.sh ${home}/tmp/anykernel.sh
-		cp ${split_img}/kernel ${home}/tmp/Image
-		${bin}/7za a -tzip $backup_package ${home}/META-INF ${bin} ${home}/LICENSE ${home}/tmp/anykernel.sh ${home}/tmp/Image ${home}/vendor_dlkm.img
-		rm -rf ${home}/tmp
+		${bin}/7za a -tzip -bd $backup_package \
+			${home}/META-INF ${bin} ${home}/LICENSE ${home}/_restore_anykernel.sh ${split_img}/kernel ${home}/vendor_dlkm.img
+		${bin}/7za rn -bd $backup_package kernel Image
+		${bin}/7za rn -bd $backup_package _restore_anykernel.sh anykernel.sh
+		sync
 
 		ui_print " "
 		ui_print "- The current kernel and vendor_dlkm have been backedup to:"
@@ -172,6 +172,8 @@ else
 	fi
 
 	ui_print "- Unpacking /vendor_dlkm partition..."
+	extract_vendor_dlkm_dir=${home}/_extract_vendor_dlkm
+	mkdir -p $extract_vendor_dlkm_dir
 	vendor_dlkm_is_ext4=false
 	extract_erofs ${home}/vendor_dlkm.img $extract_vendor_dlkm_dir || vendor_dlkm_is_ext4=true
 	sync
@@ -184,6 +186,7 @@ else
 		umount $extract_vendor_dlkm_dir
 
 		[ "$vendor_dlkm_free_space" -gt 10240 ] || {
+			# Resize vendor_dlkm image
 			ui_print "- /vendor_dlkm partition does not have enough free space!"
 			ui_print "- Trying to resize..."
 			super_free_space=$(${bin}/lptools_static free | grep '^Free space' | awk '{print $NF}')
@@ -204,7 +207,7 @@ else
 			unset super_free_space vendor_dlkm_current_size_mb vendor_dlkm_target_size_mb
 		}
 
-		ui_print "- Trying to mount as read-write..."
+		ui_print "- Trying to mount vendor_dlkm image as read-write..."
 		mount ${home}/vendor_dlkm.img $extract_vendor_dlkm_dir -o rw -t ext4 || \
 			abort "! Failed to mount vendor_dlkm.img as read-write!"
 
@@ -213,7 +216,7 @@ else
 		extract_vendor_dlkm_modules_dir=${extract_vendor_dlkm_dir}/vendor_dlkm/lib/modules
 	fi
 
-	ui_print "- Updating /vendor_dlkm partition..."
+	ui_print "- Updating /vendor_dlkm image..."
 	cp -f ${home}/_modules/*.ko ${extract_vendor_dlkm_modules_dir}/
 	blocklist_expr=$(echo $no_needed_kos | awk '{ printf "-vE \^\("; for (i = 1; i <= NF; i++) { if (i == NF) printf $i; else printf $i "|"; }; printf "\)" }')
 	mv -f ${extract_vendor_dlkm_modules_dir}/modules.load ${extract_vendor_dlkm_modules_dir}/modules.load.old
@@ -234,10 +237,11 @@ else
 			echo 'vendor_dlkm/lib/modules/vertmp 0 0 0644' >> ${extract_vendor_dlkm_dir}/config/vendor_dlkm_fs_config
 		cat ${extract_vendor_dlkm_dir}/config/vendor_dlkm_file_contexts | grep -q 'lib/modules/vertmp' || \
 			echo '/vendor_dlkm/lib/modules/vertmp u:object_r:vendor_file:s0' >> ${extract_vendor_dlkm_dir}/config/vendor_dlkm_file_contexts
-		ui_print "- Repacking /vendor_dlkm partition..."
+		ui_print "- Repacking /vendor_dlkm image..."
 		rm -f ${home}/vendor_dlkm.img
 		mkfs_erofs ${extract_vendor_dlkm_dir}/vendor_dlkm ${home}/vendor_dlkm.img || \
 			abort "! Failed to repack the vendor_dlkm image!"
+		rm -rf ${extract_vendor_dlkm_dir}
 	fi
 
 	unset vendor_dlkm_is_ext4 vendor_dlkm_free_space extract_vendor_dlkm_dir extract_vendor_dlkm_modules_dir blocklist_expr
